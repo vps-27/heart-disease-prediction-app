504fb37b51449d1be2cca9bb3ac535286c5e174b
vps-27 <vishnupriyasrivathsan@gmail.com>
2025-11-02 01:06:19 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,38 @@
+import streamlit as st
+import numpy as np
+import pickle
+
+st.title("❤️ Heart Disease Prediction App")
+
+model = pickle.load(open("heart_disease_model.pkl", "rb"))
+scaler = pickle.load(open("scaler.pkl", "rb"))
+
+st.header("Enter Patient Details")
+
+age = st.number_input("Age", 1, 120, 45)
+sex = st.selectbox("Sex", ["Male", "Female"])
+cp = st.selectbox("Chest Pain Type", [0, 1, 2, 3])
+trestbps = st.number_input("Resting Blood Pressure", 80, 200, 120)
+chol = st.number_input("Cholesterol", 100, 600, 200)
+fbs = st.selectbox("Fasting Blood Sugar > 120 mg/dl", [0, 1])
+restecg = st.selectbox("Resting ECG results", [0, 1, 2])
+thalach = st.number_input("Max Heart Rate Achieved", 50, 250, 150)
+exang = st.selectbox("Exercise Induced Angina", [0, 1])
+oldpeak = st.number_input("ST Depression", 0.0, 10.0, 1.0)
+slope = st.selectbox("Slope", [0, 1, 2])
+ca = st.selectbox("Major Vessels", [0, 1, 2, 3])
+thal = st.selectbox("Thalassemia", [0, 1, 2, 3])
+
+sex = 1 if sex == "Male" else 0
+
+user_input = np.array([[age, sex, cp, trestbps, chol, fbs, restecg,
+                        thalach, exang, oldpeak, slope, ca, thal]])
+
+scaled_input = scaler.transform(user_input)
+prediction = model.predict(scaled_input)
+
+if st.button("Predict"):
+    if prediction[0] == 1:
+        st.error("⚠️ The model predicts that the person is **likely to have heart disease.**")
+    else:
+        st.success("✅ The model predicts that the person is **unlikely to have heart disease.**")
